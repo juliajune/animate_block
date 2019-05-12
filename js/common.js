e0141043cc56005d019c76b97aa8ad0cec2d5bd2
juliajune <julia22@bk.ru>
2019-05-12 17:16:41 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	
+	$(".column4").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_11").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_12").animated("flipInY", "fadeOutDown");
+	$(".animation_13").animated("fadeInRight", "fadeOutDown");
+
+	$(".animation_21").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_22").animated("fadeInRight", "fadeOutDown");
+
+	
+});
